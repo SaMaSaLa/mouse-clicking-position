3f24b0e0aef22a9b55c9982376158c7a41d73467
saivanga <tosaivanga@gmail.com>
2021-06-26 12:39:33 -0700

    Updated UI and added Prettier support. fixes #1, #2, #3

--- mouse-clicking-position.js
+++ mouse-clicking-position.js
@@ -3,8 +3,8 @@ let xValueEl = document.getElementById("x-position-value");
 let yValueEl = document.getElementById("y-position-value");
 
 let xYPosition = (e) => {
-    xValueEl.textContent = e.offsetX;
-    yValueEl.textContent = e.offsetY;
+  xValueEl.textContent = e.offsetX;
+  yValueEl.textContent = e.offsetY;
 };
 
 otherSpace.addEventListener("click", xYPosition);
