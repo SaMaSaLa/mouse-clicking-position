c775a40f02306eeb891a0c0598f35f627b2b882b
saivanga <tosaivanga@gmail.com>
2021-06-26 10:22:25 -0700

    Made Better

--- mouse-clicking-position.js
+++ mouse-clicking-position.js
@@ -1,8 +1,10 @@
 let otherSpace = document.getElementById("other-space");
 let xValueEl = document.getElementById("x-position-value");
 let yValueEl = document.getElementById("y-position-value");
+
 let xYPosition = (e) => {
-    xValueEl.textContent = e.clientX;
-    yValueEl.textContent = e.clientY;
+    xValueEl.textContent = e.offsetX;
+    yValueEl.textContent = e.offsetY;
 };
+
 otherSpace.addEventListener("click", xYPosition);
